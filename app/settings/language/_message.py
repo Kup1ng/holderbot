dc1711ce9d4c6015ba05ad36627f93798df0c066
Mahan <not.kup1ng@gmail.com>
2025-08-03 19:22:14 +0330

    Update _message.py

--- app/settings/language/_message.py
+++ app/settings/language/_message.py
@@ -6,10 +6,8 @@ class _MessageSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", case_sensitive=True, extra="ignore"
     )
-
     START: str = (
-        f"Welcome to HolderBot 🤖 [<code>{__version__}</code> by @ErfJabs]\n"
-        "<b><a href='https://t.me/pingihostbot'>نصب پنل و انجام تانل به صورت کامل خودکار!</a></b>"
+        f"Welcome to HolderBot 🤖 [<code>{*version*}</code> by @ErfJabs]"
     )
     LETS_BACK: str = "Let's back..."
     ITEMS_MENU: str = "Select a item or create a new:"
